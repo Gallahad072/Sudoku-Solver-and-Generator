3475923646d5b4da92c644ae3a0cba609ae105c3
Gallahad072 <gallahad072@gmail.com>
2022-06-27 02:20:00 +0100

    solve can check for uniqueness

--- sudoku.py
+++ sudoku.py
@@ -5,17 +5,6 @@ import random
 class Sudoku:
     def __init__(self) -> None:
         self.grid = [
-            [3, 0, 6, 5, 0, 8, 4, 0, 0],
-            [5, 2, 0, 0, 0, 0, 0, 0, 0],
-            [0, 8, 7, 0, 0, 0, 0, 3, 1],
-            [0, 0, 3, 0, 1, 0, 0, 8, 0],
-            [9, 0, 0, 8, 6, 3, 0, 0, 5],
-            [0, 5, 0, 0, 9, 0, 6, 0, 0],
-            [1, 3, 0, 0, 0, 0, 2, 5, 0],
-            [0, 0, 0, 0, 0, 0, 0, 7, 4],
-            [0, 0, 5, 2, 0, 6, 3, 0, 0],
-        ]
-        self.grid = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -26,19 +15,18 @@ class Sudoku:
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
         ]
-        self.display()
+        self.solutions = 0
+        self.unique = True
 
     def gen(self):
-        empty = 0
         grid_poss = {}
         for y in range(9):
             for x in range(9):
                 if self.grid[y][x] == 0:
                     grid_poss[(x, y)] = [
-                        i + 1 for i in range(9) if self.possible(x, y, i + 1)
+                        i for i in range(1, 10) if self.possible(x, y, i)
                     ]
-                    empty += 1
-        if empty:
+        if len(grid_poss) > 0:
             min_len = min(len(v) for v in grid_poss.values())
             if min_len == 0:
                 return
@@ -52,6 +40,7 @@ class Sudoku:
             return
         self.display()
         input("")
+        self.unique = True
 
     def display(self):
         print(np.matrix(self.grid))
@@ -77,9 +66,13 @@ class Sudoku:
                             self.solve()
                             self.grid[y][x] = 0
                     return
+        self.solutions += 1
+        if self.solutions > 1:
+            self.unique = False
+            return
         self.display()
-        input("Another Solution?")
 
 
 s = Sudoku()
-s.gen()
+s.solve()
+print(s.unique)
